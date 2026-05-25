99c8acd4490793abc4544f932c4986a1a44b706b
chayan25bce10620-code <chayan.25bce10620>
2026-05-25 23:34:10 +0530

    initiated variables

--- Address_Printer.cpp
+++ Address_Printer.cpp
@@ -1,5 +1,15 @@
+// address printer test
+// claude roadmap. Project description:
+// task1: Declare 4 variables (int, double, char, bool) on the stack. - done
+// task2: Print each variable's value and its memory address using &. 
+// task3: Create a pointer to each
+// task4: Modify each variable through its pointer and print again.
+// task5: Create a reference to the int and show that changing the reference changes the original
+// refrences - &
+
 #include <iostream>
-int main() {
-    std::cout<<"Hello World!";
-    return 0;
-}
+int integer=9;
+bool boolean=false;
+double doble=0.87;
+char charizard="charmander";
+
