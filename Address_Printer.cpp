a84f54e9fbbfc715ae89294553a6dc10fb99e06a
chayan25bce10620-code <chayan.25bce10620>
2026-05-25 23:46:16 +0530

    task 2 day 1

--- Address_Printer.cpp
+++ Address_Printer.cpp
@@ -5,11 +5,17 @@
 // task3: Create a pointer to each
 // task4: Modify each variable through its pointer and print again.
 // task5: Create a reference to the int and show that changing the reference changes the original
-// refrences - &
+// refrences - int&
+// pointers - int*
 
 #include <iostream>
 int integer=9;
 bool boolean=false;
 double doble=0.87;
-char charizard="charmander";
+char charizard='c';
+// char charizard="charmander"; <- char is one character stupid, also char needs single quotes '' 
+int main() {
+std::cout<<"integer:"<<integer<<"\nboolean:"<<boolean<<"\ndouble:"<<doble<<"\nchar:"<<charizard;
+std::cout<<"integer memory:"<<&integer<<"\nboolean memory:"<<&boolean<<"\ndouble memory:"<<&doble<<"\nchar memory:"<<&charizard;
 
+return 0;}
