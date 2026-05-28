25221730bde2153002874c5ba619a433f37eeb0a
chayan25bce10620-code <chayan.25bce10620>
2026-05-28 23:55:24 +0530

    task 3 day4

--- Day4/Const_audit.cpp
+++ Day4/Const_audit.cpp
@@ -1,9 +1,9 @@
 // Const Audit
 // claude roadmap. Project description:
 //task1 : Take your Day 3 Logger. Make every function that doesn't modify state const. - done in day 3 last
-//task2 : Add a get_filename() const method. 
-//task3 : Then write a free function that takes a const Logger& and calls get_filename() — confirm it compiles. 
-//task4 : Try calling a non-const method on a const reference — read the compiler error carefully. 
+//task2 : Add a get_filename() const method. - done
+//task3 : Then write a free function that takes a const Logger& and calls get_filename() — confirm it compiles. - done
+//task4 : Try calling a non-const method on a const reference — read the compiler error carefully. - done
 //task5 : Add a const int MAX_LINES = 100 and enforce it in write().
 
 #include <fstream>
@@ -30,8 +30,13 @@ class Logger {
     };
 };
 
+void pF(const Logger& log) {
+    std::cout<<"logging to:"<<log.get_filename()<<"\n";
+}
+
 int main() {
     {std::unique_ptr<Logger> log=std::make_unique<Logger>("log.txt");
+    pF(*log);
     log->write("hello from unique_ptr");};
     std::cout << "past the block, file should be closed now\n";
     
