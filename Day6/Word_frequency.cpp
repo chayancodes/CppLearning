8ae871ee7625b281e7b9ae755f1614f9860e7a44
chayan25bce10620-code <chayan.25bce10620>
2026-05-30 01:42:30 +0530

    reduced para, task 2, day 6

--- Day6/Word_frequency.cpp
+++ Day6/Word_frequency.cpp
@@ -8,6 +8,24 @@
 // task6: Log the output to a file using your Day 3 Logger (held as unique_ptr).
 
 #include <iostream>
+#include <sstream>
+#include <deque>
+#include <unordered_map>
 
-std::string para="The city woke before sunrise, not because it had to, but because it never really slept. Delivery bikes hissed through rain-dark streets while apartment windows flickered on one by one like a slow electrical heartbeat. Somewhere above the traffic, an old radio played a song nobody under forty could name, yet everyone somehow recognized. Arjun stood at the bus stop with a paper cup warming his hands, watching fog roll between buildings as if the entire skyline was exhaling. For a few quiet minutes, before notifications and deadlines and conversations started crashing into the day, the world felt strangely unfinished—in a good way, like something important was still being written.";
-
+std::string para="The city woke before sunrise, not because it had to, but because it never really slept. Delivery bikes hissed through rain-dark streets while apartment windows flickered on one by one like a slow electrical heartbeat. Somewhere above the traffic, an old radio played a song nobody under forty could name, yet everyone somehow recognized. ";
+std::istringstream stream(para);
+std::deque<std::string> words;
+std::unordered_map<std::string, int> frequency;
+std::string word;
+int main() {
+    while (stream>>word) { // this reads one word at a time
+        words.push_back(word);
+    };
+    for (auto word:words) {
+        frequency[word]++;
+    };
+    // temp code to check if unordered map is workin:
+    for (auto it = frequency.begin(); it != frequency.end(); ++it) { 
+    std::cout << it->first << " : " << it->second << "\n";
+    }
+}
